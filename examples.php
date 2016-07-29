c046c434c9838e35df3aa624297adbf18f0bb2f1
Rafael Fernandes <magnuzguerra@gmail.com>
2016-07-28 23:12:39 -0300

    removed examples.php in favor for tests/tests.php

--- examples.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'xt.php';
-include 'parsedown/parsedown.php';
-if(isset($_REQUEST['demo'])) {
-	XT\parse('examples/'.$_REQUEST['demo']);
-} else {
-	XT\parse('examples/basic.xt');
-}
--- tests/tests.php
+++ tests/tests.php
@@ -1,13 +1,13 @@
 <?php
 
-include 'xt.php';
+include '../xt.php';
 
 // ATX test
 $t = microtime(true);
 if (isset($_REQUEST['test'])) {
-	XT\parse(new SplFileObject('tests/'.$_REQUEST['test'].'.xt'));
+	XT\parse(new SplFileObject($_REQUEST['test'].'.xt'));
 } else {
-	XT\parse(new SplFileObject('examples/basic.xt'));
+	XT\parse(new SplFileObject('../examples/basic.xt'));
 }
 $t = microtime(true) - $t;
 echo "Executado em $t segundos.\n";
